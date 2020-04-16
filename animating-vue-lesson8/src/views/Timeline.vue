8015e3b646acadb25ca625e29ff31a3d5fdd1e0c
betocostadev <robertomarx@gmail.com>
2020-04-16 08:42:57 -0300

    completed

---
+++ animating-vue-lesson8/src/views/Timeline.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <img class="runner first" src="../assets/runner.png" alt="runner" />
+    <img class="runner second" src="../assets/runner.png" alt="runner" />
+    <img class="runner third" src="../assets/runner.png" alt="runner" />
+  </div>
+</template>
+
+<script>
+import gsap from 'gsap'
+
+export default {
+  mounted() {
+    // timeline will go here
+    let tl = gsap.timeline({ repeat: -1, repeatDelay: 1 })
+    tl.to('.first', { x: 800, duration: 4, ease: 'expo.out' })
+    tl.to('.second', { x: 800, duration: 5, ease: 'expo.out' }, '<')
+    tl.to('.third', { x: 800, duration: 3, ease: 'expo.out' }, '-=4.6')
+  }
+}
+</script>
+
+<style scoped>
+.runner {
+  display: block;
+  height: 5em;
+  width: 5em;
+  margin-top: 1.5em;
+}
+</style>
